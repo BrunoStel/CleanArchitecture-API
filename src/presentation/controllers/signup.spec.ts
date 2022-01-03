b73b4a373d6ade0170e324dffcced0f04d4c4cad
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-03 11:09:59 -0300

    test: ensuse SignupController returns 400 if no password and passwordConfirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -35,4 +35,38 @@ describe('signup controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissinParamError('email'))
   })
+
+  it('should return status 400 if no password is provided', () => {
+    const sut = new SignupController()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@any.com',
+        name: 'any_name',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissinParamError('password'))
+  })
+
+  it('should return status 400 if no passwordConfirmation is provided', () => {
+    const sut = new SignupController()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@any.com',
+        name: 'any_name',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissinParamError('passwordConfirmation'))
+  })
 })
