834729e8a68067f1c33d2200131c7b397cd23833
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-06 18:18:58 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://brunostel:784539126@cluster0.d0no9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  port: process.env.PORT || 5050
+}
