f21e0235887fd3c645ec2bbbf0e7ce4a939d911e
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-06 09:41:08 -0300

    test: ensure AccountMongoRepository returns an account on succes

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,43 @@
+import { IAddAccountRepository } from '../../../../data/usecases/protocols/IAddAccountRepositoyProtocol'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+interface ITypeSut {
+  sut: IAddAccountRepository
+}
+
+const makeSut = (): ITypeSut => {
+  const sut = new AccountMongoRepository()
+
+  return {
+    sut
+  }
+}
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_mail',
+      password: 'hashed_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_mail')
+    expect(account.password).toBe('hashed_password')
+  })
+})
