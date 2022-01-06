04d005d80a421c08fb028d234a7bd5846b696fcc
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-06 09:41:00 -0300

    feat: ensure AccountMongoRepository returns an account on succes

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,22 @@
+import { IAddAccountRepository } from '../../../../data/usecases/protocols/IAddAccountRepositoyProtocol'
+import { IAccountModel } from '../../../../domain/models/IAccountModel'
+import { IAddAccountModel } from '../../../../domain/usecases/protocols/IAddAccount'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements IAddAccountRepository {
+  async add (accountData: IAddAccountModel): Promise<IAccountModel> {
+    const accountCollection = MongoHelper.getConnection('accounts')
+
+    const result = await accountCollection.insertOne(accountData)
+
+    const { insertedId: id } = result
+
+    const accountById = await accountCollection.findOne({ _id: id })
+
+    const { _id, ...accountWithoutId } = accountById
+
+    const account = Object.assign({}, accountWithoutId, { id: _id.toHexString() }) as IAccountModel
+
+    return account
+  }
+}
