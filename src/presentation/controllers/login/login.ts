5b8013151b632649e3a956c118fe37361307f087
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-10 10:19:41 -0300

    test: ensure LoginController returns 400 if no passowrd is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,4 +1,5 @@
 import { MissinParamError } from '../../errors'
+import { ok } from '../../helpers/http-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols'
 
 export class LoginController implements IController {
@@ -6,12 +7,13 @@ export class LoginController implements IController {
     const params = ['email', 'password']
 
     for (const param of params) {
-      if (!httpRequest[param]) {
+      if (!httpRequest.body[param]) {
         return {
           statusCode: 400,
           body: new MissinParamError(param)
         }
       }
     }
+    return ok('any_data')
   }
 }
