154286f4e1dc1763d288704f72dacd78716e57ff
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-10 11:08:12 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,10 +1,12 @@
+import { IAuthentication } from '../../../domain/usecases/protocols/IAuthentication'
 import { InvalidParamError, MissinParamError } from '../../errors'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { IController, IEmailValidator, IHttpRequest, IHttpResponse } from '../../protocols'
 
 export class LoginController implements IController {
   constructor (
-    private readonly emailValidator: IEmailValidator
+    private readonly emailValidator: IEmailValidator,
+    private readonly authentication: IAuthentication
   ) {
     this.emailValidator = emailValidator
   }
@@ -13,16 +15,20 @@ export class LoginController implements IController {
     try {
       const requiredFields = ['email', 'password']
 
+      const { email, password } = httpRequest.body
+
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissinParamError(field))
         }
       }
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
-        return badRequest(new InvalidParamError(httpRequest.body.email))
+        return badRequest(new InvalidParamError(email))
       }
-      return ok('any_data')
+      const token = await this.authentication.auth(email, password)
+
+      return ok(token)
     } catch (error) {
       return serverError(error)
     }
