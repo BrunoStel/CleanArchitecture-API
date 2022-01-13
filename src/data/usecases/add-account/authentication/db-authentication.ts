207e92d2fe6beda5c814d3e7ef0b9d57419218ce
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-13 16:09:55 -0300

    feat: ensure DbAuthenticationUseCase calls HashCompare  with correct password

--- src/data/usecases/add-account/authentication/db-authentication.ts
+++ src/data/usecases/add-account/authentication/db-authentication.ts
@@ -1,13 +1,23 @@
 import { IAuthentication, IAuthenticationModel } from '../../../../domain/usecases/protocols/IAuthentication'
+import { IHashComparer } from '../../protocols/IHashComparer'
 import { IloadAccountByEmailRepository } from '../../protocols/IloadAccountByEmailRepository'
 
 export class DbAuthentication implements IAuthentication {
-  constructor (private readonly loadAccountByEmailRepository: IloadAccountByEmailRepository) {
+  constructor (
+    private readonly loadAccountByEmailRepository: IloadAccountByEmailRepository,
+    private readonly hashCompare: IHashComparer
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashCompare = hashCompare
   }
 
   async execute (authenticationModel: IAuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(authenticationModel.email)
-    return null
+    const account = await this.loadAccountByEmailRepository.load(authenticationModel.email)
+
+    if (!account) {
+      return null
+    }
+
+    await this.hashCompare.compare(authenticationModel.password, account.password)
   }
 }
