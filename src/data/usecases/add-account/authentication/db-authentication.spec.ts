f06cf8f2e8eca2bb49d1a653461cc753d1bc69fb
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-13 16:10:01 -0300

    test: ensure DbAuthenticationUseCase calls HashCompare  with correct password

--- src/data/usecases/add-account/authentication/db-authentication.spec.ts
+++ src/data/usecases/add-account/authentication/db-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { IAccountModel } from '../db-add-account-Protocols'
 import { IloadAccountByEmailRepository } from '../../protocols/IloadAccountByEmailRepository'
 import { DbAuthentication } from './db-authentication'
+import { IHashComparer } from '../../protocols/IHashComparer'
 
 class LoadAccountByEmailRepositoryStub implements IloadAccountByEmailRepository {
   async load (email: string): Promise<IAccountModel> {
@@ -8,22 +9,31 @@ class LoadAccountByEmailRepositoryStub implements IloadAccountByEmailRepository 
       id: 'any_id',
       name: 'any_name',
       email: 'any_email@mail.com',
-      password: 'any_password'
+      password: 'hashed_password'
     }
     return account
+  }
+}
+
+class HashCompareStub implements IHashComparer {
+  async compare (value: string, hash: string): Promise<boolean> {
+    return true
   }
 }
 
 interface ISut {
   sut: DbAuthentication
   loadAccountByEmailRepositoryStub: LoadAccountByEmailRepositoryStub
+  hashCompareStub: HashCompareStub
 }
 const makeSut = (): ISut => {
   const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
-  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+  const hashCompareStub = new HashCompareStub()
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub, hashCompareStub)
   return {
     sut,
-    loadAccountByEmailRepositoryStub
+    loadAccountByEmailRepositoryStub,
+    hashCompareStub
   }
 }
 
@@ -59,6 +69,18 @@ describe('DbAuthenticationUseCase', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     })
-    expect(acessToken).toBe(null)
+    expect(acessToken).toBeNull()
+  })
+  it('Should call HashCompare  with correct password', async () => {
+    const { sut, hashCompareStub } = makeSut()
+
+    const compareSpy = jest.spyOn(hashCompareStub, 'compare')
+
+    await sut.execute({
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
 })
