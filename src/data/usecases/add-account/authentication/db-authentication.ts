873c7b1e0b15ffc322ad95ac31723a09d5a70bf1
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-15 10:39:50 -0300

    feat: ensure DbAuthenticationUseCase throw if TokenGenerator throws

--- src/data/usecases/add-account/authentication/db-authentication.ts
+++ src/data/usecases/add-account/authentication/db-authentication.ts
@@ -1,14 +1,17 @@
 import { IAuthentication, IAuthenticationModel } from '../../../../domain/usecases/protocols/IAuthentication'
 import { IHashComparer } from '../../protocols/IHashComparer'
 import { IloadAccountByEmailRepository } from '../../protocols/IloadAccountByEmailRepository'
+import { ITokenGenerator } from '../../protocols/ITokenGenerator'
 
 export class DbAuthentication implements IAuthentication {
   constructor (
+    private readonly tokenGeneratorStub: ITokenGenerator,
     private readonly loadAccountByEmailRepository: IloadAccountByEmailRepository,
     private readonly hashCompare: IHashComparer
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashCompare = hashCompare
+    this.tokenGeneratorStub = tokenGeneratorStub
   }
 
   async execute (authenticationModel: IAuthenticationModel): Promise<string> {
@@ -18,9 +21,14 @@ export class DbAuthentication implements IAuthentication {
       return null
     }
 
-    const passwordValidation = await this.hashCompare.compare(authenticationModel.password, account.password)
-    if (!passwordValidation) {
+    const passwordComparison = await this.hashCompare.compare(authenticationModel.password, account.password)
+
+    if (!passwordComparison) {
       return null
     }
+
+    const acessToken = await this.tokenGeneratorStub.generate()
+
+    return acessToken
   }
 }
