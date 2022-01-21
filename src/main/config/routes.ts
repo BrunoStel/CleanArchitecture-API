5c15e46c14ee67211d4d1786a73237e9992fb365
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-21 12:37:57 -0300

    refactor: change routes file

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,17 +1,9 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
-// import SignupPost from '../routes/signup-routes'
+
+import Login from '../routes/login-routes'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async file =>
-    (await import(`../../../${file}`)).default(router)
-  )
+  Login(router)
 }
-
-// export default (app: Express): void => {
-//   const router = Router()
-//   app.use('/api', router)
-//   SignupPost(router)
-// }
