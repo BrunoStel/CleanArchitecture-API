df0d5350cb7628d38c331fac11d40989e72e1449
Bruno Stelmastchuk <brunostel@gmail.com>
2022-01-24 14:59:52 -0300

    chore: connect api to mongo atlas

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api' || 'mongodb+srv://brunostel:784539126@cluster0.d0no9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://brunostel:784539126@cluster0.d0no9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5h'
 }
